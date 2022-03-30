bc3aa42cb795d7b3dbc7e486a047e18a82cb1d40
João Inácio <egnawake@gmail.com>
2022-03-30 23:44:03 +0100

    Use name setter in Enemy constructor

--- MyGame/Enemy.cs
+++ MyGame/Enemy.cs
@@ -8,7 +8,7 @@ namespace MyGame
 
         public Enemy(string name)
         {
-            this.name = name;
+            SetName(name);
             health = 100;
             shield = 0;
         }
