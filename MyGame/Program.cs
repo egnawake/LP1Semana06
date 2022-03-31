c0da769d685a52d82ee194797794633c5cb313a4
João Inácio <egnawake@gmail.com>
2022-03-31 11:20:10 +0100

    Fill array of enemies

--- MyGame/Program.cs
+++ MyGame/Program.cs
@@ -9,6 +9,17 @@ namespace MyGame
             // get number of enemies from user
             Console.Write("Enter number of enemies: ");
             int enemyNumber = int.Parse(Console.ReadLine());
+
+            // init array of enemies
+            Enemy[] enemies = new Enemy[enemyNumber];
+
+            // create x enemies with name given by user
+            for (int i = 0; i < enemyNumber; i++)
+            {
+                Console.Write($"Enter name for enemy {i + 1}: ");
+                string name = Console.ReadLine();
+                enemies[i] = new Enemy(name);
+            }
         }
     }
 }
